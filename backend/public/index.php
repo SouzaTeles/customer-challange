6d671bddfa2f6ec22597a488f35750d6444efab6
Lucas Teles <teleslucas@ymail.com>
2025-12-09 21:01:02 -0300

    chore: protect code using bootstrap file outside of public

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,9 +1,3 @@
 <?php
 declare(strict_types=1);
-
-define('BASE_PATH', dirname(__DIR__));
-define('PUBLIC_PATH', __DIR__);
-
-require BASE_PATH . '/vendor/autoload.php';
-
-echo $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+require dirname(__DIR__) . '/src/bootstrap.php';
